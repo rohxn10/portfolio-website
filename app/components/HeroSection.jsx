29174aeb418d2454bc8b3247cdd8e174f4d01dd2
robarca10 <rohanpg2004@gmail.com>
2024-01-23 17:59:30 +0530

    initial commit

---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function HeroSection(){
+    return( 
+    <section>
+        <h1 className="text-red-500 text-4xl font-extrabold">
+            Hi I'm Jake Brigance
+        </h1>
+        <p className="text-white text-2xl">
+            loremiiusfjbnxi  owh9hikjbdkjhsjakdp huidkshd husajdnoals dkhugokasjdo hsakjdp
+        </p>
+    </section>
+    )
+}
+
+export default HeroSection;
