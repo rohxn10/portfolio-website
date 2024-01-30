c56d18277ec1cc21c213aa03f134b391a9c8d9fd
robarca10 <rohanpg2004@gmail.com>
2024-01-30 15:30:38 +0530

    fixed some issues

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,7 +2,7 @@
 import React, {useState} from "react";  
 import Link from "next/link";
 import NavLink from "./NavLink";
-import {Bars3Icon , XmarkIcon} from "@heroicons/react/24/solid";
+// import {Bars3Icon , XmarkIcon} from "@heroicons/react/24/solid";
 
 const navLinks=[
     {
@@ -33,12 +33,16 @@ export default function Navbar() {
         <nav className="fixed top-0 left-0 right-0 bg-[#212121] bg-opacity-90">
             <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-1 mt-3 ">
                 <Link href="/" className="text-2xl md:text-4xl text-white font-bold">LOGO</Link>
-                <div className="mobile-menu block md:hidden">
+                {/* <div className="mobile-menu block md:hidden">
                     {
-                        !navbarOpen?( <button onClick={()=>setNavbarOpen(true)} className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white"><Bars3Icon className="h-5 w-5"/></button>) :( <button onClick={()=>setNavbarOpen(false)} className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-red-500"><XmarkIcon className="h-5 w-5"/></button>)
+                        !navbarOpen?( <button onClick={()=>{setNavbarOpen(true);console.log(navbarOpen)}
+                        } className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-red-500 hover:border-red-500"><Bars3Icon className="h-5 w-5"/></button>) :
+                        ( <button onClick={()=>setNavbarOpen(false)} className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-red-500">
+                        <XmarkIcon className="h-5 w-5"/>
+                        </button>)
                     }
-                    {/* {console.log(navbarOpen)} */}
-                </div>
+                    
+                </div> */}
                 <div className="menu hidden md:block md:w-auto" id="navbar">
                     <ul className="flex p-5 md:p-0 md:flex-row justify-between md:space-x-8 mt-0">
                         {navLinks.map((link,index) => 
